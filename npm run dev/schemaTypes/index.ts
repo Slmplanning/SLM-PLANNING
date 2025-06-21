e03c55d06a81c88d922e73d07777a5919e62effa
Game-Changer-hub <vulimwabravin18@gmail.com>
2025-06-21 20:42:30 +0300

    Update on blog setting sanity

--- npm run dev/schemaTypes/index.ts
+++ npm run dev/schemaTypes/index.ts
@@ -1,6 +1,6 @@
 import blockContent from './blockContent'
 import category from './category'
-import post from './post'
 import author from './author'
+import { postType } from './postType'
 
-export const schemaTypes = [post, author, category, blockContent]
+export const schemaTypes = [postType, author, category, blockContent]
